768b5aeb685028be42f233b544b03af7c443f27d
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-10-11 02:11:35 +0530

    modify scheduled job to use environment variable for job schedule time

--- jobs/fetchCrypto.job.js
+++ jobs/fetchCrypto.job.js
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import updateCryptoData from '../services/updateCryptoData.service.js';
 // Schedule a job to run every 2 hours
 const scheduleCryptoJob = () => {
-  cron.schedule(process.env.JOB_SCHEDULE_TIME || '0 */2 * * *', () => {
+  cron.schedule(process.env.JOB_SCHEDULE_TIME, () => {
     console.log('Fetching cryptocurrency data...');
     updateCryptoData();
   });
